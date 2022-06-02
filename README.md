dab29a05d20d7d9add4e974850f7cecd70e9f8ac
libian19 <libiansimpson19@gmail.com>
2022-06-01 23:17:08 -0400

    Adelanto del 2do. taller de Calculo de descuento.

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 Repositorio para mantener los ejercicios del curso práctico de JavaScript en Platzi.
 
 ## 1er. Taller. Figuras geométricas (Cuadrado, Círculo y Triángulo)
+## Archivos: figuras.html, styleFiguras.css, figuras.js.
 - Analizar las fórmulas. 
 - Crear archivos y fórmulas con JS.
 - Implementar funciones.
-- Integrar JS con HTML y CSS
+- Integrar JS con HTML y CSS.
 - Hacer pruebas, revisiones y correcciones.
+
+## 2do. Taller. Calcular Precio de un producto con descuento, usando cupones.
+## Archivos: descuentoOpciones.html, descuentoOpciones.js. 
+## Pendiente implementar archivo de estilos.
+- Analizar las fórmulas.
+-Crear archivos y fórmulas con JS.
+- Implementar funciones.
+- Integrar JS CON HTML.
+- Hacer pruebas, revisiones y correcciones.
+
 
---
+++ descuentoOpciones.js
@@ -0,0 +1,28 @@
+function PrecioAPagar (precioRealProducto, descuento){
+    const porcentajePrecioApagar = 100 - descuento;
+    const precioConDescuento = precioRealProducto * porcentajePrecioApagar / 100;
+    
+    return(precioConDescuento);
+}
+
+function calcularPrecioAPagar (){
+    const precioProducto = document.getElementById("precioReal");
+    const precioRealProducto = parseInt(precioProducto.value);
+    const select = document.getElementById("cupones");
+    const cuponValor = select.options[select.selectedIndex].value;
+    console.log (cuponValor);
+
+    let descuento;
+    if (cuponValor === 'jdc'){
+        descuento = 30;
+    }else if(cuponValor === 'nodigas'){
+        descuento = 10;
+    }else{
+        descuento = 20;
+    }
+    console.log(descuento);
+
+    const precioConDescuento = PrecioAPagar(precioRealProducto, descuento);
+    const resultado = document.getElementById("resultadoPrecio");
+    resultado.innerText = "El precio con descuento es: $ " + precioConDescuento; 
+}
